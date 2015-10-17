c0e204d29e65df7b3d02beae786119c515029448
camcginnis <chad.a.mcginnis@gmail.com>
2015-10-16 20:10:03 -0400

    controlled up

--- public/js/controllers/ForumController.js
+++ public/js/controllers/ForumController.js
@@ -3,7 +3,7 @@ app.controller("ForumController", function($scope, $http){
 
   $scope.create = function(){
     $scope.comment.Data.completed = false;
-    $http.post("/api/forum" , $scope.formData)
+    $http.post("???" , $scope.formData)
           .then(function(response){
             $scope.commentData = {};
           });

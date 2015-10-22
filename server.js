8958f8bde96a82d9f08f7cf909e8153099d68d82
Chad McGinnis <chad.a.mcginnis@gmail.com>
2015-10-21 21:00:34 -0400

    worked on building database and created new branch

--- server.js
+++ server.js
@@ -11,53 +11,50 @@ server.get("/", function(req, res){
     res.sendFile("public/index.html");
 });
 
+//get forum
+server.get("/api/forum", function(req, res){
+  var comments = Comment.find({}, function(err, comments){
+      if(err){
+        console.log(err);
+      }
 
+      res.json(comments);
+  })
+});
+
+server.post("/api/forum", function(req, res){
+    var comment = new Comment({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      testimonial: req.body.testimonial
+    })
+
+    comment.save(function(err){
+        if(err){
+          console.log(err);
+        }
+        console.log(comment);
+        res.json(comment);
+    });
+});
+
+server.get("/api/forum/:id", function(req, res){
+    Comment.find({_id: req.params.id}, function(err, todo){
+      if(err){
+        console.log(err);
+      }
+      res.json(comment);
+    });
+});
+
+server.delete("/api/forum/:id", function(req, res){
+    Comment.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+          console.log
+        }
+        res.json({message: "Successfully deleted comment"});
+    });
+});
 
 server.listen(3030);
     console.log("You're handsome");
-
-
-    // //API Routes
-    // //get todos
-    // server.get("/api/todos", function(req, res){
-    //     var todos = Todo.find({}, function(err, todos){
-    //         if(err){
-    //             console.log(err);
-    //         }
-    //
-    //         res.json(todos);
-    //     })
-    // });
-    //
-    // server.post("/api/todos", function(req, res){
-    //     var todo = new Todo({
-    //        title: req.body.title,
-    //         body: req.body.body
-    //     });
-    //
-    //     todo.save(function(err){
-    //         if(err){
-    //             console.log(err);
-    //         }
-    //         console.log(todo);
-    //         res.json(todo);
-    //     });
-    // });
-    //
-    // server.get("/api/todos/:id", function(req, res){
-    //     Todo.find({_id: req.params.id}, function(err, todo){
-    //     if(err){
-    //       console.log(err);
-    //     }
-    //     res.json(todo);
-    //   });
-    // });
-    //
-    // server.delete("/api/todos/:id", function(req, res){
-    //     Todo.findByIdAndRemove(req.params.id, function(err){
-    //         if(err){
-    //             console.log(err);
-    //         }
-    //         res.json({message: "Successfully deleted todo"});
-    //     });
-    // });
